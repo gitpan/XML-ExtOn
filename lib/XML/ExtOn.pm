bfb2608cd309cfbb751c69982bab6fb62cf5a400
Zahatski Aliaksandr <zagap@users.sourceforge.net>
2009-01-20 19:49:21 +0000

    Import of ZAG/XML-ExtOn-0.08 from CPAN.
    gitpan-cpan-distribution: XML-ExtOn
    gitpan-cpan-version:      0.08
    gitpan-cpan-path:         ZAG/XML-ExtOn-0.08.tar.gz
    gitpan-cpan-author:       ZAG
    gitpan-cpan-maturity:     released

--- lib/XML/ExtOn.pm
+++ lib/XML/ExtOn.pm
@@ -1,6 +1,6 @@
 package XML::ExtOn;
 
-#$Id: ExtOn.pm 390 2009-01-17 14:32:43Z zag $
+#$Id: ExtOn.pm 398 2009-01-20 07:17:16Z zag $
 
 =pod
 
@@ -222,7 +222,7 @@ sub create_pipe {
 
 use base 'XML::SAX::Base';
 use vars qw( $AUTOLOAD);
-$XML::ExtOn::VERSION = '0.07';
+$XML::ExtOn::VERSION = '0.08';
 ### install get/set accessors for this object.
 for my $key (qw/ context _objects_stack _cdata_mode _cdata_characters/) {
     no strict 'refs';
